c74aa693f87b01bc125aa28b87e311ee9fa9301d
Declan Healey <declanhealey@C02D8PQ0ML7H.permutive.com>
2022-05-19 08:11:08 +0100

    Create grid for new project view

--- src/views/Projects/NewProject.js
+++ src/views/Projects/NewProject.js
@@ -1,10 +1,21 @@
 import { Component } from "react";
+import { Container, Col, Row } from "react-bootstrap";
 
 class NewProject extends Component {
   render() {
     return (
       <div>
-        <h1>New project</h1>
+        <Row>
+          <div
+            className="full-length d-none d-lg-block"
+            style={{ backgroundColor: "blue", width: "350px" }}
+          ></div>
+          <Col className="full-length" style={{ backgroundColor: "red" }}>
+            <Container style={{ backgroundColor: "green" }}>
+              <h2>Create a new project</h2>
+            </Container>
+          </Col>
+        </Row>
       </div>
     );
   }
