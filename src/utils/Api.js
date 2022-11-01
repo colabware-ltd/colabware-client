0f3dbe5e08e697e13856a9f9908300503b20d895
Declan Healey <declanhealey@C02D8PQ0ML7H.permutive.com>
2022-11-01 16:51:38 -0400

    API refactor

---
+++ src/utils/Api.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const ENTRYPOINT = process.env.REACT_APP_BACKEND_URL;
+const endpoints = {
+  project: (entrypoint, project) =>
+    `${entrypoint}/project/${encodeURI(project)}`,
+  balances: (entrypoint, address) =>
+    `${entrypoint}/project/${address}/balances`,
+  requests: (entrypoint, project, page, limit) =>
+    `${entrypoint}/project/${project}/request/list?page=${page}&limit=${limit}`,
+  paymentIntent: (entrypoint) => `${entrypoint}/user/payment-intent`,
+};
+
+export const getCsrApi = (name, ...params) =>
+  endpoints[name](ENTRYPOINT, ...params);
+
+export const requestCsr = async (name, method, headers, body, ...params) => {
+  try {
+    const response = await axios({
+      url: getCsrApi(name, ...params),
+      method,
+      headers: {
+        "Content-Type":
+          method.toUpperCase() !== "PATCH"
+            ? "application/json"
+            : "application/merge-patch+json",
+        Accept: "application/json",
+        ...headers,
+      },
+      data: body,
+    });
+    return { isError: false, data: response.data };
+  } catch (error) {
+    return {
+      isError: true,
+      data: error.response && error.response.data ? error.response.data : error,
+      error,
+    };
+  }
+};
+export const get = (name, ...params) => {
+  if (params.length === 0) params[0] = {};
+  return requestCsr(name, "GET", params[0].headers, params[0].body, ...params);
+};
+
+export const post = (name, ...params) => {
+  if (params.length === 0) params[0] = {};
+  return requestCsr(name, "POST", params[0].headers, params[0].body, ...params);
+};
